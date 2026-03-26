9d12b9d5bd4b1ca0b86fa8d9176bf6c773dd28bd
Aryan <ark.opash@gamil.com>
2026-03-26 15:35:49 +0530

    complete simple dashboard with crud

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/ui/globals.css";
+import NavLinks from "./ui/dashboard/nav-links";
 
 export const metadata: Metadata = {
   title: "NextJS Dashboard",
@@ -13,7 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex">
+        {/* Sidebar */}
+        <aside className="w-64 bg-gray-50 p-4 border-r sticky top-0 h-screen">
+          <h1 className="text-xl font-bold mb-6">Dashboard</h1>
+          <NavLinks />
+        </aside>
+
+        {/* Main Content */}
+        <main className="flex-1 p-6 bg-gray-100 min-h-screen">{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import DashboardPage from "./dashboard/page";
+
 export default function Home() {
   return (
     <div>
-
+      <DashboardPage />
     </div>
   );
 }
